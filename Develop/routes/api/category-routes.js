def51bdddd0cb540282e0bb3690ede1b2f91e35f
Brianne <brianne.ewell1@gmail.com>
2020-12-13 12:18:22 -0700

    added ability to create category, update, and delete

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,10 +7,10 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll({
+    const categoryInfo = await Category.findAll({
       include: [{model:Product}],
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(categoryInfo);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -35,14 +35,48 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+try {
+  const newCategory = Category.create(req.body);
+  res.status(200).json(newCategory);
+} catch (err) {
+  res.status(400).json(err);
+}
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+try {
+  const categoryInfo = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  });
+if (!categoryInfo[0]) {
+  res.status(404).json({message: "Could not find category with that ID"})
+  return;
+}
+res.status(200).json(categoryInfo);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+try {
+  const categoryInfo = await Category.destroy({
+    where: {
+      id: req.params.id,
+    }
+  });
+if (!categoryInfo) {
+  res.status(404).json({message: "Could not find category with that ID"})
+  return;
+}
+res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
