d75182d82610531143e3b1fc1feed25433f1a183
Brianne <brianne.ewell1@gmail.com>
2020-12-13 11:59:59 -0700

    updated tag route to find by id with product data

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+try {
+  const tagInfo = await Tag.findAll({
+    include: [{model: Product}],
+  });
+  res.status(200).json(tagInfo);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+try {
+  const tagInfo = await Tag.findByPk(req.params.id, {
+    include: [{model: Product}],
+  });
+if (!tagInfo){
+  res.status(404).json({message: "Could not find tag with that ID"});
+  return;
+}
+res.status(200).json(tagInfo);
+} catch (err) {
+  res.status(500).json(err)
+}
 });
 
 router.post('/', (req, res) => {
